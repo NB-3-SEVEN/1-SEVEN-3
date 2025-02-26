dfcf4cf03abaccdb9a8d0d19b51c93741ffccd25
funfun <alpha5179@naver.com>
2025-02-26 17:13:31 +0900

    feat: superstruct added

---
+++ src/struct.js
@@ -0,0 +1,31 @@
+import * as s from "superstruct";
+
+export const CreateParticipant = s.object({
+  nickname: s.size(s.string(), 1, 20),
+  password: s.size(s.string(), 8, 20),
+  groupId: s.min(s.integer(), 1),
+});
+
+export const CreateGroup = s.object({
+  name: s.size(s.string(), 1, 20),
+  description: s.optional(s.string()),
+  photoUrl: s.optional(s.string()),
+  goalRep: s.min(s.integer(), 1),
+  discordWebHookUrl: s.optional(s.size(), 1, 255),
+  discordInviteUrl: s.optional(s.size(), 1, 255),
+  likeCount: s.optional(s.min(s.integer), 0),
+  tags: s.array(s.string()),
+  ownerNickname: s.size(s.string(), 1, 20),
+  ownerPassword: s.size(s.string(), 8, 20),
+  badges: s.array(s.enums(["PARTICIPATION_10", "RECORD_100", "LIKE_100"])),
+  recordCount: s.optional(s.min(s.integer(), 0)),
+});
+
+export const CreateRecord = s.object({
+  exerciseType: s.enums(["RUN", "BIKE", "SWIM"]),
+  description: s.optional(s.string()),
+  time: s.min(s.integer(), 1),
+  distance: s.min(s.integer(), 0),
+  photos: s.array(s.string()),
+  authorId: s.min(s.integer(), 1),
+});

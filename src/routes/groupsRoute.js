9b03876733b141fe2bffad6b67df47a7db2f9dfa
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-27 09:44:18 +0900

    'add_group_patch'
    'update_group_patch'
    
    'setting_change_gitignore_package.json'

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -1,9 +1,34 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
+router.route("/").get((req, res) => {
   res.status(200).send("Groups route is working!");
+});
+
+router.route("/:id").patch(async (req, res) => {
+  const { id } = req.params;
+  const { ownerPassword, goalRep, ...updateData } = req.body;
+  const group = await prisma.group.findFirstOrThrow({
+    where: { id: parseInt(id, 10) },
+  });
+
+  if (group.ownerPassword !== ownerPassword) {
+    res.status(401).json({ message: "Wrong password" });
+  }
+
+  if (goalRep && !Number.isInteger(goalRep)) {
+    return res.status(400).json({ message: "goalRep must be an integer" });
+  }
+
+  const updatedGroup = await prisma.group.update({
+    where: { id: parseInt(id, 10) },
+    data: { ...updateData, goalRep },
+  });
+
+  res.json({ message: updatedGroup });
 });
 
 export default router;

5038b6aef58ad2cec5d9d06fb349822be152b844
leeminho <twomean99@gmail.com>
2025-02-27 10:16:39 +0900

    feat:그룹 추천API 추가

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -1,9 +1,47 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.status(200).send("Groups route is working!");
 });
+
+router
+  .post("/:groupId/likes", async (req, res) => {
+    const { groupId } = req.params;
+    const group = await prisma.group.findUnique({
+      where: {
+        id: parseInt(groupId),
+      },
+    });
+    const updateGroup = await prisma.group.update({
+      where: {
+        id: parseInt(groupId),
+      },
+      data: {
+        likeCount: group.likeCount + 1,
+      },
+    });
+    res.status(200).json(updateGroup);
+  })
+  .delete("/:groupId/likes", async (req, res) => {
+    const { groupId } = req.params;
+    const group = await prisma.group.findUnique({
+      where: {
+        id: parseInt(groupId),
+      },
+    });
+    const updateGroup = await prisma.group.update({
+      where: {
+        id: parseInt(groupId),
+      },
+      data: {
+        likeCount: group.likeCount - 1,
+      },
+    });
+    res.status(200).json(updateGroup);
+  });
 
 export default router;

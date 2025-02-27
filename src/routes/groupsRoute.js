c56c25ca6517ecc9572b203f17e4ea412be46a58
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-27 14:59:52 +0900

    'add_participants_post_delete`

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -46,6 +46,50 @@ router
       },
     });
     res.status(200).json(updateGroup);
+  })
+  .post("/:id/participants", async (req, res) => {
+    const { id: groupId } = req.params;
+    const { nickname, password } = req.body;
+
+    const existingParticipant = await prisma.participant.findFirst({
+      where: { nickname, groupId: parseInt(groupId, 10) },
+    });
+
+    if (existingParticipant) {
+      return res.status(400).json({ message: "nickname is required" });
+    }
+
+    const participant = await prisma.participant.create({
+      data: {
+        nickname,
+        password,
+        group: { connect: { id: parseInt(groupId, 10) } },
+      },
+    });
+
+    res.json({ message: participant });
+  })
+  .delete("/:id/participants", async (req, res) => {
+    const { id } = req.params;
+    const { nickname, password } = req.body;
+
+    const participant = await prisma.participant.findUniqueOrThrow({
+      where: { id: parseInt(id, 10) },
+    });
+
+    if (participant.nickname !== nickname) {
+      return res.status(400).json({ error: "nickname is required" });
+    }
+
+    if (participant.password !== password) {
+      return res.status(401).json({ error: "Wrong password" });
+    }
+
+    const deletedParticipant = await prisma.participant.delete({
+      where: { id: parseInt(id, 10) },
+    });
+
+    res.json({ message: deletedParticipant });
   });
 
 export default router;

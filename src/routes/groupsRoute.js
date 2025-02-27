6a46b4dd4d756758f9b66d90632db4c0634ede65
leeminho <twomean99@gmail.com>
2025-02-27 09:38:32 +0900

    feat: 그룹좋아요 기능 추가"

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -2,8 +2,40 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send("Groups route is working!");
-});
+router
+  .post("/:groupId/likes", async (req, res) => {
+    const { groupId } = req.params;
+    const group = await prisma.group.findUnique({
+      where: {
+        id: parseInt(groupId),
+      },
+    });
+    const updateGroup = await prisma.group.update({
+      where: {
+        id: parseInt(groupId),
+      },
+      data: {
+        likeCount: group.likeCount + 1,
+      },
+    });
+    res.status(200).json(updateGroup);
+  })
+  .delete("/:groupId/likes", async (req, res) => {
+    const { groupId } = req.params;
+    const group = await prisma.group.findUnique({
+      where: {
+        id: parseInt(groupId),
+      },
+    });
+    const updateGroup = await prisma.group.update({
+      where: {
+        id: parseInt(groupId),
+      },
+      data: {
+        likeCount: group.likeCount - 1,
+      },
+    });
+    res.status(200).json(updateGroup);
+  });
 
 export default router;

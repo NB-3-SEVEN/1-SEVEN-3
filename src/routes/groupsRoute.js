229f5a05ccb68d4f8d07a3e8f0218bf6df952e37
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-28 16:33:34 +0900

    'feat:asyncHandler_add'

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -3,26 +3,12 @@ import { getGroup, getGroups, postGroup } from "../api/group.js";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { CreateRecord } from "../struct.js";
 import { assert } from "superstruct";
+import { asyncHandler } from "../asyncHandler.js";
 
 const prisma = new PrismaClient();
 const router = express.Router();
 router.route("/").get(getGroups).post(postGroup);
 router.route("/:groupId").get(getGroup);
-
-const asyncHandler = (handler) => async (req, res) => {
-  try {
-    await handler(req, res);
-  } catch (e) {
-    console.error(e);
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      res.status(400).send({ message: e.message });
-    } else if (e instanceof Prisma.PrismaClientValidationError) {
-      res.status(422).send({ message: e.message });
-    } else {
-      res.status(500).send({ message: e.message });
-    }
-  }
-};
 
 // 운동 기록 등록
 router.post(
@@ -190,79 +176,91 @@ router.get(
 );
 
 router
-  .post("/:groupId/likes", async (req, res) => {
-    const { groupId } = req.params;
-    const group = await prisma.group.findUnique({
-      where: {
-        id: parseInt(groupId),
-      },
-    });
-    const updateGroup = await prisma.group.update({
-      where: {
-        id: parseInt(groupId),
-      },
-      data: {
-        likeCount: group.likeCount + 1,
-      },
-    });
-    res.status(200).json(updateGroup);
-  })
-  .delete("/:groupId/likes", async (req, res) => {
-    const { groupId } = req.params;
-    const group = await prisma.group.findUnique({
-      where: {
-        id: parseInt(groupId),
-      },
-    });
-    const updateGroup = await prisma.group.update({
-      where: {
-        id: parseInt(groupId),
-      },
-      data: {
-        likeCount: group.likeCount - 1,
-      },
-    });
-    res.status(200).json(updateGroup);
-  })
-  .patch("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { ownerPassword, goalRep, ...updateData } = req.body;
-    const group = await prisma.group.findFirstOrThrow({
-      where: { id: parseInt(id, 10) },
-    });
+  .post(
+    "/:groupId/likes",
+    asyncHandler(async (req, res) => {
+      const { groupId } = req.params;
+      const group = await prisma.group.findUnique({
+        where: {
+          id: parseInt(groupId),
+        },
+      });
+      const updateGroup = await prisma.group.update({
+        where: {
+          id: parseInt(groupId),
+        },
+        data: {
+          likeCount: group.likeCount + 1,
+        },
+      });
+      res.status(200).json(updateGroup);
+    })
+  )
+  .delete(
+    "/:groupId/likes",
+    asyncHandler(async (req, res) => {
+      const { groupId } = req.params;
+      const group = await prisma.group.findUnique({
+        where: {
+          id: parseInt(groupId),
+        },
+      });
+      const updateGroup = await prisma.group.update({
+        where: {
+          id: parseInt(groupId),
+        },
+        data: {
+          likeCount: group.likeCount - 1,
+        },
+      });
+      res.status(200).json(updateGroup);
+    })
+  )
+  .patch(
+    "/:id",
+    asyncHandler(async (req, res) => {
+      const { id } = req.params;
+      const { ownerPassword, goalRep, ...updateData } = req.body;
+      const group = await prisma.group.findFirstOrThrow({
+        where: { id: parseInt(id, 10) },
+      });
 
-    if (group.ownerPassword !== ownerPassword) {
-      res.status(401).json({ message: "Wrong password" });
-    }
+      if (group.ownerPassword !== ownerPassword) {
+        res.status(401).json({ message: "Wrong password" });
+      }
 
-    if (goalRep && !Number.isInteger(goalRep)) {
-      return res.status(400).json({ message: "goalRep must be an integer" });
-    }
+      if (goalRep && !Number.isInteger(goalRep)) {
+        return res.status(400).json({ message: "goalRep must be an integer" });
+      }
 
-    const updatedGroup = await prisma.group.update({
-      where: { id: parseInt(id, 10) },
-      data: { ...updateData, goalRep },
-    });
+      const updatedGroup = await prisma.group.update({
+        where: { id: parseInt(id, 10) },
+        data: { ...updateData, goalRep },
+      });
 
-    res.json({ message: updatedGroup });
-  })
-  .delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { ownerPassword } = req.body;
+      res.json({ message: updatedGroup });
+    })
+  )
+  .delete(
+    "/:id",
+    asyncHandler(async (req, res) => {
+      const { id } = req.params;
+      const { ownerPassword } = req.body;
 
-    const group = await prisma.group.findUniqueOrThrow({
-      where: { id: parseInt(id, 10) },
-    });
+      const group = await prisma.group.findUniqueOrThrow({
+        where: { id: parseInt(id, 10) },
+      });
 
-    if (group.ownerPassword !== ownerPassword) {
-      return res.status(401).json({ error: "Wrong password" });
-    }
+      if (group.ownerPassword !== ownerPassword) {
+        return res.status(401).json({ error: "Wrong password" });
+      }
 
-    const deletedGrop = await prisma.group.delete({
-      where: { id: parseInt(id, 10) },
-    });
+      const deletedGrop = await prisma.group.delete({
+        where: { id: parseInt(id, 10) },
+      });
 
-    res.json({ message: deletedGrop });
-  });
+      res.json({ message: deletedGrop });
+    })
+  );
 
 export default router;

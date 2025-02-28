e28023297f3b7231885e73cca777a575184c577a
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-28 09:24:19 +0900

    'feat:participants_post_delete'

--- src/routes/groupsRoute.js
+++ src/routes/groupsRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
-
 import { postGroup } from "../api/group.js";
 import { PrismaClient } from "@prisma/client";
+import { CreateParticipant } from "../struct.js";
+import { formatGroupResponse } from "../utils/groupFromatter.js";
+import { assert } from "superstruct";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -51,6 +53,8 @@ router
     const { id: groupId } = req.params;
     const { nickname, password } = req.body;
 
+    assert(req.body, CreateParticipant);
+
     const existingParticipant = await prisma.participant.findFirst({
       where: { nickname, groupId: parseInt(groupId, 10) },
     });
@@ -59,7 +63,7 @@ router
       return res.status(400).json({ message: "nickname is required" });
     }
 
-    const participant = await prisma.participant.create({
+    await prisma.participant.create({
       data: {
         nickname,
         password,
@@ -67,7 +71,29 @@ router
       },
     });
 
-    res.json({ message: participant });
+    const group = await prisma.group.findUniqueOrThrow({
+      where: { id: parseInt(groupId, 10) },
+      include: {
+        participants: {
+          select: {
+            id: true,
+            nickname: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+
+    console.log("참가자 목록:", group.participants);
+
+    const owner = group.participants.find(
+      (p) => p.nickname === group.ownerNickname
+    );
+
+    const response = formatGroupResponse(group);
+
+    res.json({ message: response });
   })
   .delete("/:id/participants", async (req, res) => {
     const { id } = req.params;

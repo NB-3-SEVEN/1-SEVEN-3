d7282572c0532f6f5b47ac428f2a4b8539d2ed12
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-03-04 09:37:32 +0900

    'bug:see_bug_fix'

--- prisma/seed.js
+++ prisma/seed.js
@@ -8,7 +8,7 @@ async function main() {
   await prisma.record.deleteMany();
   await prisma.participant.deleteMany();
   await prisma.group.deleteMany();
-  await prisma.tag.deleteMany();
+  await prisma.tags.deleteMany();
 
   await prisma.tag.createMany({
     data: TAGS,
@@ -29,7 +29,7 @@ async function main() {
       await prisma.group.update({
         where: { id: group.id },
         data: {
-          tag: {
+          tags: {
             connect: tagIds.map((id) => ({ id })),
           },
         },
